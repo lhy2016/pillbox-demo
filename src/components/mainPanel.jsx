0c933f7fd4d87817b3a3194137050ecd3b73e4d5
Arun Sunil <arun.sunil@icuro.com>
2019-11-18 13:42:59 -0800

    ,

--- src/components/mainPanel.jsx
+++ src/components/mainPanel.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import TopNav from './topNav';
 import '../css/mainPanel.css';
+import PillBox from './pillBox';
 
 class MainPanel extends Component {
     render() {
         return (<div className="main-panel"> 
                     <TopNav />
-                    
+                    <PillBox />
                 </div>);
     }
 }
--- src/components/pillBox.jsx
+++ src/components/pillBox.jsx
@@ -4,7 +4,9 @@ import '../css/pillBox.css';
 class PillBox extends Component {
     render() {
         return(
-            <div className="pillBox-container"></div>
+            <div className="pillBox-container">
+                this is pillBox Region
+            </div>
         );
     }
 }
