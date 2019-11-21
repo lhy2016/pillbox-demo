bae5d89ed5d2d07dd37a13f59f56cce032b285cd
lhy2016 <lhy920104@gmail.com>
2019-11-21 03:14:22 -0800

    .
    .

--- src/components/scheduleRow.jsx
+++ src/components/scheduleRow.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 class ScheduleRow extends Component {
     render() {
         return(
-            <div className="scheduleRow"></div>
+            <div className="scheduleRow">
+                <div className="pillName">
+                        Aspirin 5mg
+                </div>
+                <div className="Instruction">
+                    Take 2 tablets by mouth everyday
+                </div>
+                <div className="timeOfDayIconContainer">
+                    <div className="morning timeOfDay"></div>
+                </div>
+               
+            </div>
         );
     }
 }
